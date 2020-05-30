7bc3e5ef7d26a0ba84485fcb3bdc45e04418ec19
Mlgtrall <polytech.makarov.business@gmail.com>
2020-05-30 14:11:13 +0300

    HashedPassword.java update

--- src/main/java/ru/mlgtrall/jda_bot_bungee/security/HashedPassword.java
+++ src/main/java/ru/mlgtrall/jda_bot_bungee/security/HashedPassword.java
@@ -1,24 +1,22 @@
 package ru.mlgtrall.jda_bot_bungee.security;
 
 import lombok.Data;
-import lombok.Getter;
 
 @Data
 public class HashedPassword {
 
-    @Getter
-    private final String hash;
+    private String hash;
 
-    @Getter
-    private final String salt;
+    private String salt;
 
     public HashedPassword(String hash, String salt) {
         this.hash = hash;
         this.salt = salt;
     }
 
-//    public HashedPassword(String hash){
-//        this(hash, null);
-//    }
+    public HashedPassword(String salt){
+        this.hash = null;
+        this.salt = salt;
+    }
 
 }
