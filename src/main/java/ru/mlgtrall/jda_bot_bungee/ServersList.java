e4e71440d33cd29e4996c1fbe8a21448208a2883
Mlgtrall <polytech.makarov.business@gmail.com>
2020-05-30 14:00:29 +0300

    ServersList.java update to session update

--- src/main/java/ru/mlgtrall/jda_bot_bungee/ServersList.java
+++ src/main/java/ru/mlgtrall/jda_bot_bungee/ServersList.java
@@ -1,8 +1,18 @@
 package ru.mlgtrall.jda_bot_bungee;
 
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.config.ServerInfo;
+
+//TODO: make hook it bungee's config.yml
 public enum ServersList {
     MAIN("whitelist_mcfp"), LOGIN("login");
+
+    private static final Main pl = Main.getInstance();
+
     private final String name;
+
+    private final ServerInfo serverInfo;
+
 
     public static boolean isAuthorizedServer(String name){
         for (ServersList server : values()) {
@@ -12,7 +22,7 @@ public enum ServersList {
         }
         return false;
     }
-    public static boolean isLogin(String name){
+    public static boolean isLoginServer(String name){
         return ServersList.LOGIN.getName().equals(name);
     }
 
@@ -21,5 +31,10 @@ public enum ServersList {
     }
     ServersList(String name){
         this.name = name;
+        serverInfo = ProxyServer.getInstance().getServerInfo(name);
+    }
+
+    public ServerInfo getServerInfo() {
+        return serverInfo;
     }
 }
